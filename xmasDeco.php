d62e29ed5b5e3669b8ee093e77dd0b2cc35eb5bd
Per Thykjær Jensen <petj@eaaa.dk>
2020-12-01 20:15:58 +0100

    testing

---
+++ xmasDeco.php
@@ -0,0 +1,29 @@
+/*
+Plugin Name: XMAS Deco
+Plugin URI:
+Description: Will decorate your theme with xmas stuff. A reissue of a theme, but now more handy as a plugin.
+Version:     2.0
+Author:      Your name (Yay! Here comes fame... )
+Author URI:  https://github.com/asathoor/Xmas-Deco
+License:     GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+
+Copyright
+
+XMAS Deco is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+(Plugin Name) is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with (Plugin Name). If not, see (http://link to your plugin license).
+*/
+function petj_add_xmas() {
+    wp_enqueue_style( 'style-name', 'xmasBy.css' );
+}
+add_action( 'wp_enqueue_scripts', 'petj_add_xmas' );
